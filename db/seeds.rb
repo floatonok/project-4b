0102b5c488f87eea8c356e6ae7f83919746b50d4
Isabella Ong <isabellaong@gmail.com>
2016-08-05 15:32:22 +0800

    adds seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,41 +18,41 @@ if home_1.save
   puts 'Home Saved!'
 end
 
-# user_1 = User.new
-# user_1.name = 'Isabella'
-# user_1.email = 'isabella@hello.com'
-# user_1.home_id = 1
-# if user_1.save
-#   puts 'User Saved!'
-# end
-#
-# user_2 = User.new
-# user_2.name = 'Glenn'
-# user_2.email = 'glenn@hello.com'
-# user_2.home_id = 1
-# if user_2.save
-#   puts 'User Saved!'
-# end
+user_1 = User.new
+user_1.name = 'Isabella'
+user_1.email = 'isabella@hello.com'
+user_1.home_id = 1
+if user_1.save
+  puts 'User Saved!'
+end
 
-# section_grid_1 = SectionGrid.new
-# section_grid_1.occupied = true
-# section_grid_1.section_id = 1
-# section_grid_1.home_id = 1
-# if section_grid_1.save
-#   puts 'Section Grid Saved!'
-# end
-#
-# section_grid_2 = SectionGrid.new
-# section_grid_2.occupied = true
-# section_grid_2.section_id = 1
-# section_grid_2.home_id = 1
-# if section_grid_2.save
-#   puts 'Section Grid Saved!'
-# end
+user_2 = User.new
+user_2.name = 'Glenn'
+user_2.email = 'glenn@hello.com'
+user_2.home_id = 1
+if user_2.save
+  puts 'User Saved!'
+end
 
-# section_1 = Section.new
-# section_1.height = 10
-# section_1.width = 20
-# if section_1.save
-#   puts 'Section Saved!'
-# end
+section_grid_1 = SectionGrid.new
+section_grid_1.occupied = true
+section_grid_1.section_id = 1
+section_grid_1.home_id = 1
+if section_grid_1.save
+  puts 'Section Grid Saved!'
+end
+
+section_grid_2 = SectionGrid.new
+section_grid_2.occupied = true
+section_grid_2.section_id = 1
+section_grid_2.home_id = 1
+if section_grid_2.save
+  puts 'Section Grid Saved!'
+end
+
+section_1 = Section.new
+section_1.height = 10
+section_1.width = 20
+if section_1.save
+  puts 'Section Saved!'
+end
